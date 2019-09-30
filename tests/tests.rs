42277e4c9f84aef39946cf1677d2da8c07a31b5f
Sylvain BONNEFON <sbonnefo@student.42.fr>
2019-09-30 19:39:35 +0200

    start to add correction tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -243,3 +243,34 @@ fn test_no_error_consecutive_x_not_supported() {
 }
 
 //test : make && ./computer_v1 " ++++----+++-2x= 0"
+
+#[test]
+fn correction() {
+	assert_eq!("Reduced form: 0 = 0\n\
+				Every ℝéels numbers can be the solution.",
+				solve("5 * X^0 = 5 * X^0"));
+//     assert_eq!(Polynomial::from_str("5 * X^0 = 5 * X^0").unwrap_or_default().get_non_zero_constant(),
+//                Some(NonZeroConstant::SolubleEverywhere));
+//     assert_eq!(Polynomial::from_str("3 * X^0 = 8 * X^0").unwrap_or_default().get_non_zero_constant(),
+//                Some(NonZeroConstant::Unsoluble));
+//     assert_eq!(Polynomial::from_str("5 * X^0 = 4 * X^0 + 7 * X^1").unwrap_or_default().get_linear(),
+//                Some(Linear::Soluble(0.14285714285714285)));
+//     assert_eq!(Polynomial::from_str("5 * X^0 + 13 * X^1 + 3 * X^2 = 1 *X^0 + 1 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Positive(-0.3670068381445481, -3.632993161855452)));
+//     assert_eq!(Polynomial::from_str("6 * X^0 + 11 * X^1 + 5 * X^2 = 1 * X^0 + 1 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Null(-1.0)));
+//     assert_eq!(Polynomial::from_str("5 * X^0 + 3 * X^1 + 3 * X^2 = 1 * X^0 + 0 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Negative(3.0, 3.0)));
+//     assert_eq!(Polynomial::from_str("5 * X^0 - 5 * X^0").unwrap_or_default().get_non_zero_constant(),
+//                Some(NonZeroConstant::SolubleEverywhere));
+//     assert_eq!(Polynomial::from_str("3 * X^0 - 8 * X^0").unwrap_or_default().get_non_zero_constant(),
+//                Some(NonZeroConstant::Unsoluble));
+//     assert_eq!(Polynomial::from_str("5 * X^0 - 4 * X^0 - 7 * X^1").unwrap_or_default().get_linear(),
+//                Some(Linear::Soluble(0.14285714285714285)));
+//     assert_eq!(Polynomial::from_str("5 * X^0 + 13 * X^1 + 3 * X^2 - 1 *X^0 - 1 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Positive(-0.3670068381445481, -3.632993161855452)));
+//     assert_eq!(Polynomial::from_str("6 * X^0 + 11 * X^1 + 5 * X^2 - 1 * X^0 - 1 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Null(-1.0)));
+//     assert_eq!(Polynomial::from_str("5 * X^0 + 3 * X^1 + 3 * X^2 - 1 * X^0 - 0 * X^1").unwrap_or_default().get_quadratic(),
+//                Some(Quadratic::Negative(3.0, 3.0)));
+}
